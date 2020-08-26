29c69fa938173bacc10ea6581c62e8acd8385fbd
rctauber <rctauber@gmail.com>
2020-08-26 08:30:21 -0700

    Extend GECKO with Maelstrom terms, move all external terms to external sheet

--- src/ihcc_view.py
+++ src/ihcc_view.py
@@ -11,7 +11,17 @@ top_levels = [
     {"ID": "GECKO:0000056", "Label": "questionnaire/survey data"},
     {"ID": "GECKO:0000101", "Label": "statistics"},
     {"ID": "GECKO:0000105", "Label": "other", "Parent": "genomics"},
-    {"ID": "GECKO:0000112", "Label": "other biosample type", "Parent": "sample type"}
+    {"ID": "GECKO:0000112", "Label": "other biosample type", "Parent": "sample type"},
+    {
+        "ID": "GECKO:0000116",
+        "Label": "other questionnaire/survey data",
+        "Parent": "questionnaire/survey data",
+    },
+    {
+        "ID": "GECKO:0000117",
+        "Label": "perception of health and quality of life",
+        "Parent": "questionnaire/survey data",
+    },
 ]
 
 
